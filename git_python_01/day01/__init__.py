ea5ea5d8e34726a71fea41dde7fe1066efab256e
cxy01 <cxy01@163.com>
2022-06-22 16:50:48 +0800

    cxy01提交

---
+++ git_python_01/day01/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+"""
+__init__.py.py
+Created on 2022/06/22 0022 下午 04:46
+Copyright (c) 2022/06/22 0022, xxx公司版权所有.
+@author: 法外狂徒——张三
+"""
---
+++ git_python_01/day01/cxy01.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+"""
+cxy01.py
+Created on 2022/06/22 0022 下午 04:47
+Copyright (c) 2022/06/22 0022, xxx公司版权所有.
+@author: 法外狂徒——张三
+"""
+
+a = 1
+b = 2
